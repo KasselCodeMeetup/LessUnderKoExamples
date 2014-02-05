a16a5205243edef56e9cb92fb095ae4068f4c276
Daniel Huhn <daniel_huhn@outlook.com>
2014-02-05 09:58:49 +0100

    improved the knockout example

--- knockoutExamples/example.js
+++ knockoutExamples/example.js
@@ -1,18 +1,31 @@
+function Person(name, age, job){
+    this.name = ko.observable(name);
+    this.age = ko.observable(age);
+    this.job = ko.observable(job);
+}
+
 function ViewModel(){
     var self = this;
     
     self.name = ko.observable();
-    self.items = ko.observableArray();
     self.selectedItem = ko.observable();
+
+    self.items = ko.observableArray([
+        new Person("Max", 23, "Entwickler"),
+        new Person("Jessica", 22, "Bankkauffrau"),
+        new Person("Julia", 19, "Alternpflegerin"),
+        new Person("Robert", 24, "Bankkaufmann"),
+        new Person("Dennis", 27, "Mechantroniker")
+    ]);
+    
     self.itemsCount = ko.computed(function() {
         return self.items().length;
     });
 
     self.addItem = function () {
-        self.items.push({
-            name: Math.random() > 0.5 ? "Reichard" : "Heinz",
-            number: Math.floor(Math.random() * 10000000)
-        });
+        var newPerson = new Person("<Name>", "<Alter>", "<Job>")
+        self.items.push(newPerson);
+        self.selectedItem(newPerson);
     };
 }
 
